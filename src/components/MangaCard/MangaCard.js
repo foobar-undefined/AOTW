67b96aa6ca968b6795bcf7e23e0108afbb8e5bda
foobar-undefined <mxfb1997@gmail.com>
2023-05-17 14:52:27 -0400

    wip: MangaCard added

--- src/components/MangaCard/MangaCard.js
+++ src/components/MangaCard/MangaCard.js
@@ -1,2 +1,14 @@
 import { Link } from "react-router-dom";
 
+export default function MangaCard({manga}){
+    return(
+        <>
+            <Link to={`/manga/${manga.id}`} state={{manga:manga}} className="manga-card-link"> 
+                <div className="manga-card">
+                    <h2>{manga.attributes.canonitcalTitle}</h2>
+                    <img src = {manga.attributes.psoterImage.medium} />
+                </div>
+            </Link>
+        </>
+    )
+}
