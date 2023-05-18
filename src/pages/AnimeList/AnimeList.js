64644d5fbd03a6338d1224e4ddf249935473a921
foobar-undefined <mxfb1997@gmail.com>
2023-05-18 13:57:25 -0400

    wip: search bar functionality for anime page

--- src/pages/AnimeList/AnimeList.js
+++ src/pages/AnimeList/AnimeList.js
@@ -4,30 +4,49 @@ import AnimeCard from "../../components/AnimeCard/AnimeCard";
 
 export default function AnimeList(props){
     const [anime, setAnimeList] = useState([]);
+    const [search, setSearch] = useState("");
 
     const fetchData = async () => {
         try{
-            const response = await fetch("https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=6")
+            let url = "https://kitsu.io/api/edge/anime?"
+            
+            if(search){
+                const encodedQuery = encodeURIComponent(search);
+                url += `filter[text]=${encodedQuery}`;
+            }
+            const response = await fetch(url)
             const animeData = await response.json()
             setAnimeList(animeData.data);
         }catch(error){
             console.log(error)
         }
-    }
+    };
+
     useEffect(()=>{
         fetchData();
     },[]);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        fetchData();
+    };
+
+    const handleSearchInput = (e) => {
+        setSearch(e.target.value);
+    }
 
     return(
         <main>
             <div className='content-wrap'>
                 <div className='search-box-wrap'>
                     
-                    <form className="search-box">
+                    <form className="search-box" onSubmit={handleSearch}>
                         <input
                             type ="search"
                             placeholder="Search for an anime..."
-                            required
+                            value={search}
+                            onChange={handleSearchInput}
+                            required    
                             />
                             <button type ="submit" class="searchButton">search</button>
                     </form>
@@ -40,5 +59,5 @@ export default function AnimeList(props){
                 </div>
             </div>
         </main>
-    )
+    );
 }
