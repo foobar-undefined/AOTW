d0b6b1f9b2b25c04c7efdd6e31f5ff395e79bac3
foobar-undefined <mxfb1997@gmail.com>
2023-05-18 23:19:46 -0400

    wip: welcome page styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,17 @@
 import './Home.css'
+import imgs from "../../imgs/welcome-goku.jpg"
+
 export default function Home(){
     return(
-    <div className="center">
-        <h1>Welcome Page</h1>
+    <div className="wrapper">
+        <div className='welcome-container'
+            style = {{backgroundImage: `url(${imgs})`,
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                            height: "100vh"
+                }}> <h1> Welcome,  Please stay a while</h1>
+        </div>
     </div>
+
     )
 }
